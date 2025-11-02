d690dd7f837af7e8887abdf5d87dfec1ec760cb0
Alex H <alex.huang@sjsu.edu>
2025-11-01 21:28:37 -0700

    Add create user and login

---
+++ app/auth.py
@@ -0,0 +1,43 @@
+import bcrypt
+import constants
+import jwt
+
+from datetime import datetime, timedelta, timezone
+from pymongo import MongoClient
+from jwt.exceptions import InvalidTokenError
+from pwdlib import PasswordHash
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+client = MongoClient(constants.MONGO_SVR)
+
+def generate_jwt_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, constants.SECRET_KEY, algorithm=constants.ALGORITHM)
+    return encoded_jwt
+
+def hash_password(password: str) -> str:
+  salt = bcrypt.gensalt()
+  return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+def verify_password(password: str, hashed: bytes) -> bool:
+  return bcrypt.checkpw(password.encode("utf-8"), hashed.encode("utf-8"))
+
+def authenticate_user(username: str, password: str) -> bool:
+  try:
+    db = client.get_database("CloudAnalytics")
+    users_collection = db.get_collection("users")
+    user = users_collection.find_one({"username": username})
+    if user and verify_password(password, user['password']):
+      return True
+    else:
+      return False
+  except Exception as e:
+    raise Exception("Unable to find the document due to the following error: ", e)
+
+    
--- app/main.py
+++ app/main.py
@@ -1,11 +1,58 @@
-from typing import Union
-from fastapi import FastAPI
+from typing import Union, Annotated
+from datetime import timedelta
+
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+import auth
+import constants
+from constants import Token, TokenData
+
+
 
 app = FastAPI(title="Cloud Analytics", version="1.0.0")
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to FastAPI Authentication Demo"}
+
+@app.get("/server")
+async def ping_server():
+    try:
+        auth.client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+@app.post("/user/create")
+async def create_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    try:
+        auth.client.get_database("CloudAnalytics").get_collection("users").insert_one({
+            "username": form_data.username,
+            "password": auth.hash_password(form_data.password)
+        })
+        return {"message": "User created successfully"}
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Unable to create user due to the following error: " + str(e)
+        )
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    if not auth.authenticate_user(form_data.username, form_data.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=constants.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = auth.generate_jwt_token(
+        data={"user": form_data.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
 
 
 @app.get("/items/{item_id}")
