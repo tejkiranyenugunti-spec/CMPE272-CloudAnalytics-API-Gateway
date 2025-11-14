8d14bf4fe8a3e7bd54b5fdf4b4eed845d640a35e
Tej Kiran Yenugunti <tejkiranyenugunti@gmail.com>
2025-11-14 15:29:00 -0800

    Update MongoDB connection string in constants.py

--- app/constants.py
+++ app/constants.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 
 SECRET_KEY = "e9e1f7e2ec67ea20f33924d972677f268f006d1e0b70160cb56c25ee836bc1f6"
-MONGO_SVR = "mongodb+srv://alexhuang_db_user:9Yq56XrrSk4xeVHe@cluster0.pxkwsh5.mongodb.net/?appName=Cluster0"
+MONGO_SVR = "mongodb+srv://Tej122:vial111@cluster0.7nhjunh.mongodb.net/CloudAnalytics?retryWrites=true&w=majority"
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 ALGORITHM = "HS256"
